bb5d33e0cc60a59b2b96214afc2c4c86ba4bc274
Adrian Vargas <adrivara2018@gmail.com>
2024-10-03 02:48:09 -0600

    library

--- ids/datasets/preprocessing.py
+++ ids/datasets/preprocessing.py
@@ -9,7 +9,16 @@ def load_and_preprocess_data(file_path, target_column):
     """
     Carga y preprocesa el conjunto de datos desde el archivo especificado.
     """
-    df = pd.read_csv(file_path)
+    # Cargar el dataset con el separador adecuado
+    df = pd.read_csv(file_path, sep=';')
+
+    # Crear la columna 'Success' basada en 'G3'
+    df['Success'] = (df['G3'] > 10).astype(int)
+
+    # Eliminar las columnas 'G1', 'G2' y 'G3' si existen
+    df.drop(columns=['G1', 'G2', 'G3'], errors='ignore', inplace=True)
+
+    # Separar características y variable objetivo
     y = df[target_column]
     X = df.drop(columns=[target_column])
 
@@ -18,16 +27,18 @@ def load_and_preprocess_data(file_path, target_column):
     numerical_cols = X.select_dtypes(include=['int64', 'float64']).columns.tolist()
 
     # Discretizar variables numéricas
-    discretizer = KBinsDiscretizer(n_bins=3, encode='ordinal', strategy='uniform')
-    X[numerical_cols] = discretizer.fit_transform(X[numerical_cols])
+    if numerical_cols:
+        discretizer = KBinsDiscretizer(n_bins=3, encode='ordinal', strategy='uniform')
+        X[numerical_cols] = discretizer.fit_transform(X[numerical_cols])
 
     # Aplicar One-Hot Encoding a variables categóricas
-    X_encoded = pd.get_dummies(X, columns=categorical_cols)
+    if categorical_cols:
+        X = pd.get_dummies(X, columns=categorical_cols)
 
     # Convertir todos los datos a tipo 'float' para evitar problemas
-    X_encoded = X_encoded.astype(float)
+    X = X.astype(float)
 
-    return X_encoded, y
+    return X, y
 
 def balance_data(X, y):
     """
